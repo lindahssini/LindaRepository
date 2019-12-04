da762077f228817106ccf681e247f692272675a1
Linda Hssini <lhssini@iodomain.com>
2019-12-04 16:24:29 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Configuration';
+export class AppComponent  {
+ 
+  hide = true;
+  Certificate:boolean=false;
+  User:boolean=true;
+  IsDisabledUA:boolean=true;
+ 
+  selecteditemUA:string="anonymous";
+  onChangeUA(event)
+  {
+   
+    if(this.selecteditemUA=="username")
+    {
+      this.Certificate=true;
+    this.User=false;
+  this.IsDisabledUA=false;
+  }
+  else if(this.selecteditemUA=="certificate")
+  {
+    this.Certificate=false;
+    this.User=true;
+  this.IsDisabledUA=false;
+  }else{
+    this.IsDisabledUA=true;
+    this.Certificate=false;
+    this.User=true;
+  }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule, MatOptionModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
 
+import {
+   
+    MatDialogModule,
+  
+    MatInputModule,
+    MatButtonModule
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      BrowserAnimationsModule,
+      MatFormFieldModule,
+      MatSelectModule,
+      MatCheckboxModule,
+      BrowserModule,
+      MatOptionModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MatTableModule,
+      MatDialogModule,
+      MatIconModule,
+      MatInputModule,
+      MatButtonModule,
+      MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
